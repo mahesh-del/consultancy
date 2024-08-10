20d4cb93acb90ba880e395648d483524fde798ab
mahesh-del <maheshkusuma1999@gmail.com>
2024-08-10 20:56:24 +0530

    Added career details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { ContactComponent } from './components/contact/contact.component';
 import { AppComponent } from './app.component';
 import { CareersComponent } from './components/careers/careers.component';
 import { BusinessComponent } from './components/business/business.component';
+import { CareerDetailsComponent } from './components/career-details/career-details.component';
 
 export const routes: Routes = [
-    {path:'',component:LandingpageComponent},
-    {path:'contact',component:ContactComponent},
-    {path:'careers',component:CareersComponent},
-    {path:'business-process-consulting',component:BusinessComponent},
+    { path: '', component: LandingpageComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'careers', component: CareersComponent },
+    { path: 'careers/:title', component: CareerDetailsComponent },
+    { path: 'business-process-consulting', component: BusinessComponent },
 
 ];
